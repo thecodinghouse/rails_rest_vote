69cead71e70d74a8d9a69467d5a8e4fc7b3fc816
abhinav <abhinav.sohani@consultadd.in>
2016-04-05 19:14:52 +0530

    vote object in response

--- app/controllers/rails_rest_vote/api/votes_controller.rb
+++ app/controllers/rails_rest_vote/api/votes_controller.rb
@@ -1,5 +1,8 @@
+require "json"
+
 module RailsRestVote
   class Api::VotesController < ApplicationController
+
     skip_before_action :verify_authenticity_token
     before_action :authenticate_user!, :only=>[:up ,:down,:exists]
     before_action :exists, :only=>[:up ,:down]
@@ -28,13 +31,13 @@ module RailsRestVote
       user = User.find(params[:user_id])
       upvotes = user.votes.where(:vote => true)
       downvotes = user.votes.where(:vote => false)
-      render(json: { status: 200, upcount: upvotes.size , downcount: downvotes.size })
+      render(json: { status: 200, upcount: upvotes.size ,upvotes:JSON.parse(upvotes.to_json), downcount: downvotes.size,downvotes: JSON.parse(downvotes.to_json) })
     end
 
     def model_vote_count
       upvotes = Vote.where("votable_id = ? AND votable_type = ? AND vote = true",params[:votable_id],params[:votable_type].capitalize)
       downvotes = Vote.where("votable_id = ? AND votable_type = ? AND vote = false",params[:votable_id],params[:votable_type].capitalize)
-      render(json: { status: 200, upcount: upvotes.size , downcount: downvotes.size })
+      render(json: { status: 200, upcount: upvotes.size ,upvotes:JSON.parse(upvotes.to_json), downcount: downvotes.size,downvotes: JSON.parse(downvotes.to_json) })
     end
 
     private
