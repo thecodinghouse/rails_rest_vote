9ca2d00e917af44f7d9b23a2bff7eeb29e278827
Satendra Rai <satendrarai5@gmail.com>
2018-01-02 17:44:15 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  #namespace :rails_rest_vote do
-    #namespace :api do
-      post 'api/votes/up' => 'rails_rest_vote/api/votes#up' #API for voting up
-      post 'api/votes/down' => 'rails_rest_vote/api/votes#down' #API for voting down
-      post 'api/likes' => 'rails_rest_vote/api/votes#like' #API for like unlike feature
-      get 'api/votes/user' => 'rails_rest_vote/api/votes#user_vote_count' #API for vote count done by a particular user
-      get 'api/votes/model' => 'rails_rest_vote/api/votes#model_vote_count' #API for vote count done on particular model
-      get 'api/likes/user' => 'rails_rest_vote/api/votes#user_like_count' #API for like count done by a particular user
-      get 'api/likes/model' => 'rails_rest_vote/api/votes#model_like_count' #API for like count done on particular model
-    #end
-  #end
+    post 'api/votes/up' => 'rails_rest_vote/api/votes#up' #API for voting up
+    post 'api/votes/down' => 'rails_rest_vote/api/votes#down' #API for voting down
+    post 'api/likes' => 'rails_rest_vote/api/votes#like' #API for like unlike feature
+    get 'api/votes/user' => 'rails_rest_vote/api/votes#user_vote_count' #API for vote count done by a particular user
+    get 'api/votes/model' => 'rails_rest_vote/api/votes#model_vote_count' #API for vote count done on particular model
+    get 'api/likes/user' => 'rails_rest_vote/api/votes#user_like_count' #API for like count done by a particular user
+    get 'api/likes/model' => 'rails_rest_vote/api/votes#model_like_count' #API for like count done on particular model
 end
